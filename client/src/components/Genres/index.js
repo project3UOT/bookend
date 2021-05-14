1a0b78121481301a56630fd96c7bf0348e81f434
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-14 10:46:15 -0400

    hero input style

--- client/src/components/Genres/index.js
+++ client/src/components/Genres/index.js
@@ -6,13 +6,16 @@ const Genres = () => {
     return (
         <section className='section'>
             <h2 className='has-text-centered is-family-secondary is-size-3 text-dark mb-2'>Or, pick a genre</h2>
+            <div className='buttons is-centered'>
+
             {genres.map(genre =>
                 <button
-                    className='button is-large is-success m-2'
-                    key={genre}
+                className='button is-large is-success m-2'
+                key={genre}
                 >
                     {genre}
                 </button>)}
+            </div>
         </section>
     );
 };
--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -4,16 +4,22 @@ const Hero = () => {
     return (
         <section className='hero is-secondary is-medium is-relative'>
             <img className='hero-bg' src={require('../../assets/imgs/book-bg.png')} alt='books' />
-            
-            <div className='hero-body move-to-front columns'>
-                <div className='column is-two-thirds is-offset-2'>
 
-                <h2 className='has-text-centered is-family-secondary is-size-3 has-background-primary p-4 mb-2'>Haven't read a book since highschool English class? Watched everything on Netflix? Enter your favourite TV show, and we'll recommend some books for you.</h2>
-                
-                <div className='is-flex-direction-row has-text-centered'>
-                    <input className='input hero-input is-large mr-2' type='text' />
-                    <button className='button is-primary is-large'>Go</button>
-                </div>
+            <div className='hero-body move-to-front columns is-centered'>
+                <div className='column is-two-thirds'>
+
+                    <h2 className='has-text-centered is-family-secondary is-size-3 has-background-primary p-4 mb-2'>Haven't read a book since highschool English class? Watched everything on Netflix? Enter your favourite TV show, and we'll recommend some books for you.</h2>
+                    <div className='is-flex-direction-column is-align-items-center'>
+
+                        <div className='field has-addons'>
+                            <div className='control'>
+                                <input className='input is-large mr-2' type='text' placeholder='Enter a TV show or film' />
+                            </div>
+                            <div className='control'>
+                                <button className='button is-primary is-large'>Go</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
