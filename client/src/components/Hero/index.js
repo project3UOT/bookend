660af201d5db29e9a7fbe9b56ec4ebb5edb9665a
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-14 15:53:55 -0400

    hero responsive

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -6,9 +6,9 @@ const Hero = () => {
             <img className='hero-bg' src={require('../../assets/imgs/book-bg.png')} alt='books' />
 
             <div className='hero-body move-to-front columns is-centered'>
-                <div className='column is-two-thirds'>
+                <div className='column is-two-thirds is-full-mobile'>
 
-                    <h2 className='has-text-centered is-family-secondary is-size-3 has-background-primary p-4 mb-2'>Haven't read a book since highschool English class? Watched everything on Netflix? Enter your favourite TV show, and we'll recommend some books for you.</h2>
+                    <h2 className='has-text-centered is-family-secondary is-size-3 is-size-5-touch has-background-primary p-4 mb-2 is-full-mobile'>Haven't read a book since highschool English class? Watched everything on Netflix? Enter your favourite TV show, and we'll recommend some books for you.</h2>
                     <div className='is-flex-direction-column is-align-items-center'>
 
                         <div className='field has-addons'>
