f6ca80eb358361bcd9b0323a15c7d209e7b37174
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-14 08:25:53 -0400

    genres section

--- client/src/components/Genres/index.js
+++ client/src/components/Genres/index.js
@@ -4,16 +4,16 @@ const genres = ['Horror', 'Comedy', 'Sci-Fi', 'Fantasy', 'Thriller', 'Romance', 
 
 const Genres = () => {
     return (
-        // <section className='section'>
-        //     <h2 className='has-text-centered is-family-secondary is-size-3 text-dark mb-2'>Or, pick a genre</h2>
-        //     {genres.map(genre =>
-        //         <button
-        //             className='button is-large is-success m-2'
-        //             key={genre}
-        //         >
-        //             {genre}
-        //         </button>)}
-        // </section>
+        <section className='section'>
+            <h2 className='has-text-centered is-family-secondary is-size-3 text-dark mb-2'>Or, pick a genre</h2>
+            {genres.map(genre =>
+                <button
+                    className='button is-large is-success m-2'
+                    key={genre}
+                >
+                    {genre}
+                </button>)}
+        </section>
     );
 };
 
