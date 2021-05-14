0bbb41ec2041ef24b5a62a1b34c070d259ad0e19
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-14 09:19:32 -0400

    modal format

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -9,53 +9,59 @@ import Auth from '../../utils/auth';
 const AppNavbar = () => {
   // set modal display state
   const [showModal, setShowModal] = useState(false);
+  const [showLogin, setShowLogin] = useState(true);
 
   return (
     <>
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className='navbar-brand'>
-        <a className="navbar-item" href="/">
-          Book End
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className='navbar-brand'>
+          <a className="navbar-item" href="/">
+            Book End
         </a>
-      </div>
+        </div>
 
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <a className="button is-primary" onClick={() => setShowModal(true)}>
-              <strong>Login</strong>
-            </a>
-            <a className="button is-primary" onClick={() => setShowModal(true)}>
-              <strong>Sign up</strong>
-            </a>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary" onClick={() => setShowModal(true)}>
+                <strong>Login</strong>
+              </a>
+              <a className="button is-primary" onClick={() => setShowModal(true)}>
+                <strong>Sign up</strong>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
 
-    </nav>
+      </nav>
 
-    {/* <div class="modal">
-  <div class="modal-background"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-            <div class="tabs">
+      <div className={showModal ? 'modal is-active' : 'modal'}>
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head is-justify-content-space-between is-align-items-flex-start">
+            <div className="tabs">
               <ul>
-                <li class="is-active"><a>Login</a></li>
-                <li><a>Sign Up</a></li>
+                <li className={showLogin ? 'is-active' : ''}>
+                  <a onClick={() => setShowLogin(true)}>Login</a>
+                </li>
+                <li>
+                  <a className={!showLogin ? 'is-active' : ''} onClick={() => setShowLogin(false)}>Sign Up</a>
+                </li>
               </ul>
             </div>
-      <button class="delete" aria-label="close"></button>
-    </header>
-    <section class="modal-card-body">
-      Content
-    </section>
-    <footer class="modal-card-foot">
-      <button class="button is-success">Start Reading!</button>
-      <button class="button">Cancel</button>
-    </footer>
-  </div>
-</div> */}
-</>
+            <button className="delete" aria-label="close"></button>
+          </header>
+          <section className="modal-card-body">
+            {showLogin ? <LoginForm handleModalClose={() => setShowModal(false)} /> :
+              <SignUpForm handleModalClose={() => setShowModal(false)} />}
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-success">Start Reading!</button>
+            <button className="button">Cancel</button>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 };
 
