de68e2f9ffd2d7cf694e323484ea63f89446c19b
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-15 16:35:37 -0400

    profilenav

--- client/src/components/ProfileNav/index.js
+++ client/src/components/ProfileNav/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useBookendContext } from "../../utils/GlobalState";
 import { UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { FaFilter } from "react-icons/fa";
 
 const ProfileNav = () => {
     const [state, dispatch] = useBookendContext();
+
+    const [showMobileNav, setShowMobileNav] = useState(false);
 
     const { categories, currentCategory } = state;
 
@@ -12,26 +15,35 @@ const ProfileNav = () => {
             type: UPDATE_CURRENT_CATEGORY,
             currentCategory: categories[index]
         });
-        console.log(state);
     };
 
-
     return (
-        <nav className='has-background-secondary p-4 is-family-secondary' role='navigation' aria-label='profile navigation'>
-                <div className='buttons are-medium'>
+        <nav className='navbar buttons has-background-secondary p-4 is-family-secondary' role='navigation' aria-label='profile navigation'>
+            
+                    <a
+                        role='button'
+                        className='is-flex-mobile is-hidden-tablet pb-2'
+                        aria-label='filter'
+                        aria-expanded='false'
+                        onClick={() => { setShowMobileNav(!showMobileNav) }}>
+                        <FaFilter />
+                    </a>
+                
+                <div className={'buttons are-medium' + (showMobileNav ? ' is-flex is-justify-content-center' : ' is-hidden-mobile')}>
                     {categories.map((category, index) => {
                         return (
-                        <button 
-                            className={
-                                'button' +
-                                    (category === currentCategory ? ' is-primary is-rounded is-outlined' : ' is-secondary')}
-                            key={category}
-                            onClick={() => {handleClick(index)}}>
+                            <button
+                                className={
+                                    'button is-rounded is-size-6-mobile' +
+                                    (category === currentCategory ? ' is-outlined is-primary' : ' is-secondary')}
+                                key={category}
+                                onClick={() => { handleClick(index) }}>
                                 {category}
-                        </button>
+                            </button>
                         )
-                    })}                    
+                    })}
                 </div>
+            
         </nav>
     );
 };
