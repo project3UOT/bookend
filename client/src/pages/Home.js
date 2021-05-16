3d7018e9406f1639c58bcd390407fa2e0790f3e4
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-05-15 20:34:09 -0400

    partseed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Hero from '../components/Hero/index';
 import Genres from '../components/Genres';
-
+import { idbPromise} from '../utils/helpers'
 const Home = () => {
+    useEffect(()=> {
+        idbPromise("booksToRead","get").then(results=>{
+            console.log(results)
+        })
+
+    },[])
     return (
         <>
         <Hero />
--- server/models/Book.js
+++ server/models/Book.js
@@ -1,10 +1,11 @@
 const { Schema } = require('mongoose');
+const mongoose = require("mongoose")
 
 // This is a subdocument schema, it won't become its own
 // model but we'll use it as the schema for the User's 
 //`savedBooks` array in User.js
 
-const EXAMPLE = new Schema({
+const bookSchema = new Schema({
   authors: [
     {
       type: String,
@@ -30,4 +31,6 @@ const EXAMPLE = new Schema({
   },
 });
 
-module.exports = bookSchema;
+const Book = mongoose.model("Book", bookSchema)
+
+module.exports = Book;
