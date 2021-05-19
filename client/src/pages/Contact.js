682071affd5f4040fe67a6d8e8e4826aaeb02d0e
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-19 19:22:58 -0400

    coming soon tecxt

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <p className='text-dark'>Contact</p>
+        <>
+            <h2 className='text-dark is-size-2 is-family-secondary'>Contact Us</h2>
+            <p className='text-dark'>coming soon</p>
+        </>
     );
 };
 
