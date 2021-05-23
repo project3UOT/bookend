47adaecfd67cbe0a6085d0540debe64284474fb0
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-05-23 18:31:43 -0400

    fallbackimg and delete mutation;

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import BookCardFooter from '../BookCardFooter';
 
-const BookCard = ({ bookId, title, author, img, read, favourite, fromSearch, saved }) => {
+const BookCard = ({ bookId, title, author, img, read, favourite, fromSearch, saved }) => {    
     return (
         <div className='column is-2-widescreen is-one-fifth-desktop is-4-tablet'>
 
         <div className='card'>
-            <div className='card-image'>
-                    <img src={img || 'https://via.placeholder.com/500x500'} alt={`${title} book cover`}/>
-            </div>
+                
+                <div className='card-image'>
+                    <img src={img || require('../../assets/imgs/logoblue.png')} alt={`${title} book cover`}/>
+                </div>
+                
                 <div className='card-content'>
+                    <div>
+
                     <p className='title is-family-secondary is-size-4'>{title}</p>
                     <p className='subtitle is-size-6'>{author}</p>
+                    </div>
+                    
                 </div>
             
             <BookCardFooter 
